9f21eac0793f12e0692598de7bb636809dd358f6
yessasikiran <sxs73800@ucmo.com>
2023-04-11 19:42:40 -0500

    version 1

---
+++ create-topic.py
@@ -0,0 +1,8 @@
+import boto3
+
+# Initialize the client
+sns_client = boto3.client('sns', region_name='us-east-1')
+
+# Create an SNS topic
+topic_name = 'my-topic1024'
+response = sns_client.create_topic(Name=topic_name)
